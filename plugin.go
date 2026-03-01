578169e4ff712d29d7ca141edb9a8c4c177b6bd9
marnaldoss <marnaldo@sorint.com>
2026-03-01 12:01:51 +0100

    fix

--- plugin.go
+++ plugin.go
@@ -1,4 +1,4 @@
-package keycloakauth
+package main
 
 import (
 	"net/http"
@@ -14,14 +14,14 @@ func (p *KeycloakAuthPlugin) Name() string {
 }
 
 func (p *KeycloakAuthPlugin) Version() string {
-	return "1.2.35"
+	return "1.2.36"
 }
 
 func (p *KeycloakAuthPlugin) Register(app core.App) error {
 	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
 		se.Router.GET("/keycloakauth/version", func(e *core.RequestEvent) error {
 			return e.JSON(http.StatusOK, map[string]string{
-				"version": "1.2.35",
+				"version": "1.2.36",
 			})
 		})
 		return se.Next()
