e2137b116457558c60005c44c295ff8b02492d78
marnaldoss <marnaldo@sorint.com>
2026-03-01 11:53:05 +0100

    first commit

---
+++ plugin.go
@@ -0,0 +1,33 @@
+package keycloakauth
+
+import (
+	"net/http"
+
+	"github.com/paccolamano/plugincmd"
+	"github.com/pocketbase/pocketbase/core"
+)
+
+type KeycloakAuthPlugin struct{}
+
+func (p *KeycloakAuthPlugin) Name() string {
+	return "keycloakauth"
+}
+
+func (p *KeycloakAuthPlugin) Version() string {
+	return "1.2.35"
+}
+
+func (p *KeycloakAuthPlugin) Register(app core.App) error {
+	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
+		se.Router.GET("/keycloakauth/version", func(e *core.RequestEvent) error {
+			return e.JSON(http.StatusOK, map[string]string{
+				"version": "1.2.35",
+			})
+		})
+		return se.Next()
+	})
+
+	return nil
+}
+
+var Plugin plugincmd.PBPlugin = &KeycloakAuthPlugin{}
